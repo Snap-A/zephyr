538d1f84630374a0dae84ce19109176146013aac
Andreas Wolf <awolf002@gmail.com>
2025-03-30 09:29:20 -0400

    Save state for POC testing of DTS

---
+++ drivers/wifi/infineon/airoc_gpio.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2025, Andreas Wolf (awolf002@gmail.com)
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/** @file
+ *  Defines API and constants to perform GPIO operations on Wifi chip
+ */
+
+#ifndef INCLUDED_AIROC_GPIO_H_
+#define INCLUDED_AIROC_GPIO_H_
+
+#define AIROC_GPIO_LED   0
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/** Sends an IOVAR command to set a given GPIO pin to the specified level.
+ *
+ *  @param  gpio    : The pin number as seen by the Wifi chip. (See defines)
+ *  @param  on      : The logic state the pin should be set to:
+ *                    0  => Low/Off
+ *                    >0 => High/On
+ *  @return WHD_SUCCESS or Error code
+ */
+extern uint32_t airoc_gpio_set(uint8_t gpio, uint8_t on);
+
+#ifdef __cplusplus
+}     /* extern "C" */
+#endif
+#endif /*  INCLUDED_AIROC_GPIO_H_ */
