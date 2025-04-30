d869e3fb9b1d64d7472217cccecd5393b727cd35
Andreas Wolf <awolf002@gmail.com>
2025-04-29 21:29:21 -0400

    Snapshot for new ADC driver code

---
+++ drivers/adc/adc_txxNSxx1.c
@@ -0,0 +1,234 @@
+/*
+ * Copyright (c) 2025 Andreas Wolf
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief ADC driver for the TI xxNSxx1 ADCs.
+ *
+ * This driver supports multiple variants of the Texas Instrument ADC chip.
+ * There are differences in the number of channels, the resolution, and the 
+ * sampling speed between the versions of this chip:
+ *
+ *  ADCxx1Sxx1 : 1 channel
+ *  ADCxx2Sxx1 : 2 channels
+ *  ADCxx4Sxx1 : 4 channels
+ *
+ *  ADC08NSxx1 : 8-bit resolution
+ *  ADC10NSxx1 : 10-bit resolution
+ *  ADC12NSxx1 : 12-bit resolution
+ *
+ *  ADCxxNS021 : Up to 200 ksps
+ *  ADCxxNS051 : Up to 500 ksps
+ *  ADCxxNS101 : Up to 1 Msps
+ *
+ */
+
+#include <zephyr/drivers/adc.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/spi.h>
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/sys/byteorder.h>
+#include <zephyr/sys/util.h>
+
+LOG_MODULE_REGISTER(adc_txxNSxx1, CONFIG_ADC_LOG_LEVEL);
+
+#define MIN_RESOLUTION 8U
+#define MAX_RESOLUTION 12U
+
+struct txxNSxx1_config {
+	struct spi_dt_spec bus;
+	uint8_t channels;
+	uint8_t resolution;
+};
+
+struct txxNSxx1_data {
+	const struct device *dev;
+	uint16_t *buffer;
+	uint8_t channels;
+	uint8_t resolution;
+};
+
+static int txxNSxx1_channel_setup(const struct device *dev,
+				  const struct adc_channel_cfg *channel_cfg)
+{
+	const struct txxNSxx1_config *config = dev->config;
+
+	if (channel_cfg->gain != ADC_GAIN_1) {
+		LOG_ERR("unsupported channel gain '%d'", channel_cfg->gain);
+		return -ENOTSUP;
+	}
+
+	if (channel_cfg->reference != ADC_REF_EXTERNAL0) {
+		LOG_ERR("unsupported channel reference '%d'",
+			channel_cfg->reference);
+		return -ENOTSUP;
+	}
+
+	if (channel_cfg->acquisition_time != ADC_ACQ_TIME_DEFAULT) {
+		LOG_ERR("unsupported acquisition_time '%d'",
+			channel_cfg->acquisition_time);
+		return -ENOTSUP;
+	}
+
+	if (channel_cfg->channel_id >= config->channels) {
+		LOG_ERR("unsupported channel id '%d'", channel_cfg->channel_id);
+		return -ENOTSUP;
+	}
+
+	return 0;
+}
+
+static int txxNSxx1_read_channel(const struct device *dev, uint8_t channel,
+				 uint16_t *result)
+{
+	const struct txxNSxx1_config *config = dev->config;
+
+        /* The ADC frame length is 16 bits, but only a subset is used for real data.  */
+        /* The first frame sets the channel number for the conversion, and the second */
+        /* frame contains the obtained data for that channel. The data written in the */
+        /* second frame is no useful (selecting channel 0), and the data read in the  */
+        /* first frame might originate from an unknown channel and is ignored.        */
+
+	uint8_t tx_bytes[2];
+	uint8_t rx_bytes[2];
+	int err;
+	const struct spi_buf tx_buf[2] = {
+		{
+			.buf = tx_bytes,
+			.len = sizeof(tx_bytes)
+		},
+		{
+			.buf = NULL,
+			.len = 1
+		}
+	};
+	const struct spi_buf rx_buf[2] = {
+		{
+			.buf = NULL,
+			.len = 1
+		},
+		{
+			.buf = rx_bytes,
+			.len = sizeof(rx_bytes)
+		}
+	};
+	const struct spi_buf_set tx = {
+		.buffers = tx_buf,
+		.count = ARRAY_SIZE(tx_buf)
+	};
+	const struct spi_buf_set rx = {
+		.buffers = rx_buf,
+		.count = ARRAY_SIZE(rx_buf)
+	};
+
+	/*
+	 * Configuration byte consists of: 2 dummy bits + D2 + D1 + D0 + 3 dummy bits
+	 */
+	tx_bytes[0] = channel << 3;
+	tx_bytes[1] = 0;
+
+	err = spi_transceive_dt(&config->bus, &tx, &rx);
+	if (err) {
+		return err;
+	}
+
+	*result = sys_get_be16(rx_bytes);
+	*result &= (BIT_MASK(config->resolution) << 2);
+
+	return 0;
+}
+
+static int txxNSxx1_init(const struct device *dev)
+{
+	const struct txxNSxx1_config *config = dev->config;
+	struct txxNSxx1_data *data = dev->data;
+
+	data->dev = dev;
+
+	if (!spi_is_ready_dt(&config->bus)) {
+		LOG_ERR("SPI bus is not ready");
+		return -ENODEV;
+	}
+
+	return 0;
+}
+
+static int txxNSxx1_read(const struct device *dev,
+			 const struct adc_sequence *sequence)
+{
+	struct txxNSxx1_data *data = dev->data;
+	uint16_t result = 0;
+	uint8_t channel;
+	int err;
+
+	while (data->channels) {
+		channel = find_lsb_set(data->channels) - 1;
+
+		LOG_DBG("reading channel %d", channel);
+
+		err = txxNSxx1_read_channel(data->dev, channel, &result);
+		if (err) {
+			LOG_ERR("failed to read channel %d (err %d)",
+				channel, err);
+			break;
+		}
+
+		LOG_DBG("read channel %d, result = %d", channel,
+			result);
+
+		*data->buffer++ = result;
+		WRITE_BIT(data->channels, channel, 0);
+	}
+
+	return result;
+}
+
+static DEVICE_API(adc, txxNSxx1_adc_api) = {
+	.channel_setup = txxNSxx1_channel_setup,
+	.read = txxNSxx1_read,
+};
+
+#define INST_DT_ADCXXNSXX1(inst, t) DT_INST(inst, ti_##t)
+
+#define ADCXXNSXX1_DEVICE(t, n, ch) \
+	static const struct txxNSxx1_config ti_adc##t##_config_##n = { \
+		.bus = SPI_DT_SPEC_GET(INST_DT_ADCXXNSXX1(n, t), \
+					 SPI_OP_MODE_MASTER | SPI_TRANSFER_MSB | \
+					 SPI_WORD_SET(16), 0), \
+		.channels = ch, \
+	}; \
+	DEVICE_DT_DEFINE(INST_DT_ADCXXNSXX1(n, t), \
+			 &txxNSxx1_init, NULL, \
+			 NULL, \
+			 &ti_adc##t##_config_##n, POST_KERNEL, \
+			 CONFIG_ADC_INIT_PRIORITY, \
+			 &txxNSxx1_adc_api)
+
+/*
+ * ADCxx1Sxx1: 1 channels
+ */
+#define ADCXX1SXX1_DEVICE(n) ADCXXNSXX1_DEVICE(adcxx1Sxx1, n, 1)
+
+/*
+ * ADCxx2Sxx1: 2 channels
+ */
+#define ADCXX2SXX1_DEVICE(n) ADCXXNSXX1_DEVICE(adcxx2Sxx1, n, 2)
+
+/*
+ * ADCxx4Sxx1: 4 channels
+ */
+#define ADCXX4SXX1_DEVICE(n) ADCXXNSXX1_DEVICE(adcxx4Sxx1, n, 4)
+
+#define CALL_WITH_ARG(arg, expr) expr(arg)
+
+#define INST_DT_ADCXXNSXX1_FOREACH(t, inst_expr)	\
+	LISTIFY(DT_NUM_INST_STATUS_OKAY(ti_adcxx##t),	\
+		CALL_WITH_ARG, (;), inst_expr)
+
+INST_DT_ADCXXNSXX1_FOREACH(4Sxx1, ADCXX4SXX1_DEVICE);
+INST_DT_ADCXXNSXX1_FOREACH(2Sxx1, ADCXX2SXX1_DEVICE);
+INST_DT_ADCXXNSXX1_FOREACH(1Sxx1, ADCXX1SXX1_DEVICE);
