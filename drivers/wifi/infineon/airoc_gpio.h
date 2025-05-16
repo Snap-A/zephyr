9c7ebe3ba3ab08d12b6ed706df77c7dfaa4c4304
Andreas Wolf <awolf002@gmail.com>
2025-05-16 11:39:17 -0400

    Functioning Pico_W blinky sample

--- drivers/wifi/infineon/airoc_gpio.h
+++ drivers/wifi/infineon/airoc_gpio.h
@@ -10,7 +10,7 @@
 #ifndef INCLUDED_AIROC_GPIO_H_
 #define INCLUDED_AIROC_GPIO_H_
 
-#define AIROC_GPIO_LED   0
+#define AIROC_GPIO_LED_PIN   0
 
 #ifdef __cplusplus
 extern "C"
@@ -23,9 +23,9 @@ extern "C"
  *  @param  on      : The logic state the pin should be set to:
  *                    0  => Low/Off
  *                    >0 => High/On
- *  @return WHD_SUCCESS or Error code
+ *  @return 0 (SUCCESS) or Error code
  */
-extern uint32_t airoc_gpio_set(uint8_t gpio, uint8_t on);
+extern int airoc_gpio_set(uint8_t gpio, uint8_t on);
 
 #ifdef __cplusplus
 }     /* extern "C" */
