5e4e0b13df762392cc0c702f0ae644e817f8598c
Andreas Wolf <awolf002@gmail.com>
2025-05-16 11:39:17 -0400

    Functioning Pico_W blinky sample

--- samples/basic/blinky/src/main.c
+++ samples/basic/blinky/src/main.c
@@ -11,6 +11,13 @@
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
 
+#include <zephyr/autoconf.h>
+
+// Pico W devices use a GPIO on the WIFI chip for the LED,
+// so when building for Pico W, AIROC_GPIO_LED_PIN will be defined
+#if CONFIG_BOARD_RPI_PICO_RP2040_W
+#include <airoc_gpio.h>
+#else
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
 
@@ -19,12 +26,14 @@
  * See the sample documentation for information on how to fix this.
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+#endif
 
 int main(void)
 {
 	int ret;
 	bool led_state = true;
 
+#if defined(LED0_NODE)
 	if (!gpio_is_ready_dt(&led)) {
 		return 0;
 	}
@@ -33,14 +42,20 @@ int main(void)
 	if (ret < 0) {
 		return 0;
 	}
+#endif
 
 	while (1) {
+#if defined(LED0_NODE)
 		ret = gpio_pin_toggle_dt(&led);
+#elif defined(AIROC_GPIO_LED_PIN)
+		led_state = !led_state;
+		// Ask the wifi "driver" to set the GPIO on or off
+		ret = airoc_gpio_set(AIROC_GPIO_LED_PIN, led_state);
+#endif
 		if (ret < 0) {
 			return 0;
 		}
 
-		led_state = !led_state;
 		printf("LED state: %s\n", led_state ? "ON" : "OFF");
 		k_msleep(SLEEP_TIME_MS);
 	}
